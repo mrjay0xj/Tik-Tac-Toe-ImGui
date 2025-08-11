cbaab21de3760888455f9f53368d86b33200dac2
mrjay0xj <span.smjr.span@gmail.com>
2025-08-11 11:30:09 +0330

    the game logic is creating now

--- main.cpp
+++ main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <string>
 #include <GLFW/glfw3.h>
 #include <imgui.h>
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
+
+char board[3][3] = { {' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '}};
+char currentPlayer = 'X';
+
+bool gameEnded = false;
+
+bool checkWinner()
+{
+    for (int row{0};row < 3;++row)
+    {
+        if (board[row][0] == board[row][1] && board[row][1] == board[row][2] && board[row][0] != ' ')
+        {
+            return true;
+        }
+    }
+    
+    for (int col{0};col < 3;++col)
+    {
+        if (board[0][col] == board[1][col] && board[1][col] == board[2][col] && board[0][col] != ' ')
+        {
+            return true;
+        }
+    }
+    
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] == ' ')
+    {
+        return true;
+    }
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] == ' ')
+    {
+        return true;
+    }
+    
+    return false;
+}
 
 int main() {
     if (!glfwInit())
@@ -12,10 +48,11 @@ int main() {
     const char* glsl_version = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     // Create window with graphics context
     float main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor()); // Valid on GLFW 3.3+ only
-    GLFWwindow* window = glfwCreateWindow((int)(1280 * main_scale), (int)(800 * main_scale), "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow((int)(590 * main_scale), (int)(630 * main_scale), "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
     if (window == nullptr)
         return 1;
     glfwMakeContextCurrent(window);
@@ -26,25 +63,26 @@ int main() {
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
 
     // Setup scaling
     ImGuiStyle& style = ImGui::GetStyle();
-    style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
-    style.FontScaleDpi = main_scale;        // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
+    style.ScaleAllSizes(main_scale);
+    style.FontScaleDpi = main_scale;
 
     // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
+
+    ImGuiWindowFlags flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse;
+
     // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    
+    std::string labels[2][2] = {};
 
     while (!glfwWindowShouldClose(window))
     {
@@ -59,36 +97,44 @@ int main() {
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            ImGui::SetNextWindowPos(ImVec2(0, 0));
+            ImGui::SetNextWindowSize(ImVec2(590, 630));
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            if (ImGui::Begin("Tik Tac Toe!", nullptr, flags))
+            {
+                for (int row{0};row < 3;++row)
+                {
+                    for (int col{0};col < 3;++col)
+                    {
+                        float xPos = 10.0f + col * (100.0f + 10.0f);
+                        float oPos = 30.0f + row * (100.0f + 10.0f);
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
+                        ImGui::SetCursorPos(ImVec2(xPos,oPos));
+                        std::string btnlbl = std::string(1,board[row][col]) + "##" + std::to_string(row) + "," + std::to_string(col);
+                        if (ImGui::Button(btnlbl.c_str(), ImVec2(100.0f, 100.0f)))
+                        {
+                            if (board[row][col] == ' ')
+                            {
+                                board[row][col] = currentPlayer;
+                                if (checkWinner())
+                                {
+                                    gameEnded = true;
+                                }
+                                else
+                                {
+                                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                                }
+                            }
+                        }
+                    }
+                }
+            }    
+            if (gameEnded)
+            {
+                ImGui::SetNextWindowSize(ImVec2(150, 200));
+                ImGui::OpenPopup("Game over!");
+            }
             ImGui::End();
         }
 
